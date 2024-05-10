b66d4397a20dd68c4b35e09c891cec78eac6b14e
PratyushPoddar07 <shuvampoddar018@gmail.com>
2024-05-11 03:02:37 +0530

    moduleandpackage

---
+++ app.py
@@ -0,0 +1,8 @@
+# Importing necessary modules for image handling, IO operations, logging, web app development,
+# Google's generative AI models, and handling Google API exceptions.
+from PIL import Image  # For working with images
+import io  # For handling various types of I/O operations
+import logging  # For logging messages
+import streamlit as st  # Streamlit library for building web applications
+import google.generativeai as genai  # For interacting with Google's generative AI models
+from google.api_core import exceptions as google_exceptions  # For handling Google API exceptions
