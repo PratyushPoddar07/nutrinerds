81ff7fb439fe0bbcfdbd149b1263b2e726e9030e
PratyushPoddar07 <shuvampoddar018@gmail.com>
2024-05-11 14:21:51 +0530

    test

--- app.py
+++ app.py
@@ -16,46 +16,81 @@ generation_config = {
 
 st.set_page_config(page_title="Docify", page_icon=":gem:")
 
-st.header("Docify")
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+st.title("Docify")
+st.write('---')
 
 with st.container():
-    leftcolumn, rightcolumn = st.columns((1,2))
-    
-    with leftcolumn:
-        with st.sidebar:
-            st.title("Menu")
-            st.button("Profile")
-            st.button("new ask")
-            st.button("History")
-            st.button("Date and Time")
+        leftcoloumn, middlecolumn, rightcolumn = st.columns(3)
 
-    with rightcolumn:
-        genai.configure(api_key='Enter your API_key')
+        with rightcolumn:
+            with st.container():
+                s = st.button("signin")
+                if s == True:
+                    st.write("signed in")
+                
+                s2 = st.button("signup")
+                if s == True:
+                    st.write("signed up")
+                
+        
+            
+    
+        with leftcoloumn:
+
+            with st.sidebar:
+                st.title("Menu")
+                st.button("Home")
+                st.button("Account")
+                st.button("History")
+                st.button("About")
+
+        with middlecolumn:
+            genai.configure(api_key='ENTER YOUR API_KEY')
         select_model = st.radio("Select Type", ["Consultancy", "Image Consultancy"])
         
         if select_model == "Image Consultancy":
+            uploaded_image = st.file_uploader(
+                "upload image",
+                label_visibility="collapsed",
+                accept_multiple_files=False,
+                type=["png", "jpg"],
+                )
+        # st.caption(
+        #     "Note: The vision model gemini-pro-vision is not optimized for multi-turn chat."
+        # )
+            if uploaded_image:
+                image_bytes = uploaded_image.read()
             picture = st.camera_input("Take a picture")
 
+        # if picture:
+        #     st.image(picture)
+
             if picture is not None:
-                # Convert the picture to bytes
-                image = Image.open(io.BytesIO(picture.getvalue()))
+                image_bytes = picture.getvalue()
 
-                # Resize the image to a smaller size
-                resized_image = image.resize((100, 100))  # Adjust size as needed
 
-                # Convert the resized image back to bytes
-                resized_image_bytes = io.BytesIO()
-                resized_image.save(resized_image_bytes, format=image.format)
-                image_bytes = resized_image_bytes.getvalue()
+
+
+        
+
 
 def get_response(messages, model="gemini-pro"):
     model = genai.GenerativeModel(model)
-    res = model.generate_content(messages, generation_config=generation_config)
+    res = model.generate_content(messages,
+                                generation_config=generation_config)
     return res
+
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = []
 messages = st.session_state["messages"]
+
+# The vision model gemini-pro-vision is not optimized for multi-turn chat.
+# st.header("Docify")
+# st.write("How can I help you?")
+
 
 # Initialize session state for chat history if it doesn't exist
 if messages and select_model != "Image Consultancy":
@@ -65,9 +100,8 @@ if messages and select_model != "Image Consultancy":
             st.chat_message("user").markdown(parts[0])
         elif role == "model":
             st.chat_message("assistant").markdown(parts[0])
-
 chat = st.radio("Select one of the following options", ["Common Cold", "Influenza (Flu)", "Pneumonia", "Tuberculosis (TB)", "Hypertension", "Other"])
-if chat == "Other":
+if chat == "other":
     chat = st.text_input("Enter the disease name")
 
 st.write("How can I help you?")
@@ -80,7 +114,8 @@ if chat_message:
 
     if select_model == "Image Consultancy":
         if "image_bytes" in globals():
-            vision_message = [chat_message, Image.open(io.BytesIO(image_bytes))]
+            vision_message = [chat_message,
+                              Image.open(io.BytesIO(image_bytes))]
             try:
                 res = get_response(vision_message, model="gemini-pro-vision")
             except google_exceptions.InvalidArgument as e:
@@ -90,7 +125,7 @@ if chat_message:
                     st.error("An error occurred. Please try again.")
             except Exception as e:
                 logging.error(e)
-                st.error("Error occurred. Please refresh your page and try again.")
+                st.error("Error occured. Please refresh your page and try again.")
         else:
             vision_message = [{"role": "user", "parts": [chat_message]}]
             st.warning(
@@ -104,9 +139,11 @@ if chat_message:
                     st.error("An error occurred. Please try again.")
             except Exception as e:
                 logging.error(e)
-                st.error("Error occurred. Please refresh your page and try again.")
+                st.error("Error occured. Please refresh your page and try again.")
     else:
-        messages.append({"role": "user", "parts":  [chat_message]})
+        messages.append(
+            {"role": "user", "parts":  [chat_message]},
+        )
         try:
             res = get_response(messages)
         except google_exceptions.InvalidArgument as e:
@@ -116,7 +153,7 @@ if chat_message:
                 st.error("An error occurred. Please refresh your page and try again.")
         except Exception as e:
             logging.error(e)
-            st.error("Error occurred. Please refresh your page and try again.")
+            st.error("Error occured. Please refresh your page and try again.")
 
     if res is not None:
         res_text = ""
@@ -124,7 +161,7 @@ if chat_message:
             if chunk.candidates:
                 res_text += chunk.text
             if res_text == "":
-                res_text = "inappropriate words"
+                res_text = "unappropriate words"
                 st.error("Your words violate the rules that have been set. Please try again!")
         res_area.markdown(res_text)
 
